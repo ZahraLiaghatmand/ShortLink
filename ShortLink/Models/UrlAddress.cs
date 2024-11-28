c3ab9b25168b6910cdd8b6ddaf846c4431bdacbe
zahra liaghatmand <zahra.liaghatmand@gmail.com>
2024-11-28 13:57:26 +0330

    Add UrlService to do CRUD operation

--- ShortLink/Models/UrlAddress.cs
+++ ShortLink/Models/UrlAddress.cs
@@ -2,7 +2,9 @@
 {
     public class UrlAddress
     {
+        public int Id { get; set; }
         public required string Url { get; set; }
         public string? ShortCode { get; set; }
+        public required User Owner { get; set; }
     }
 }
---
+++ ShortLink/Services/UrlService.cs
@@ -0,0 +1,35 @@
+﻿using ShortLink.Models;
+
+namespace ShortLink.Services
+{
+    public class UrlService
+    {
+        static List<UrlAddress> Urls { get; }
+        static UrlService() 
+        {
+            Urls = new List<UrlAddress>();
+        }
+        public static List<UrlAddress> GetUrls() => Urls;
+        public static UrlAddress? GetUrlById(int id) 
+            => Urls.FirstOrDefault(u => u.Id == id );
+        public static UrlAddress? GetUrlByShortCode(string shortCode)
+            => Urls.FirstOrDefault(u => u.ShortCode == shortCode);
+        public static void AddUrl(UrlAddress url) => Urls.Add(url);
+        public static void deleteUrlByUrl(int id) 
+        { 
+            UrlAddress? urlD = GetUrlById(id);
+            if (urlD != null)
+            {
+                Urls.Remove(urlD);
+            }
+        }
+        public static void updateUrl(UrlAddress url)
+        {
+            UrlAddress? urlU = GetUrlById(url.Id);
+            if (urlU != null)
+            {
+                urlU = url;
+            }
+        }
+    }
+}
