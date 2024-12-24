f3f846b55f18ac026b836acad8ba9908127c59cb
zahra liaghatmand <zahra.liaghatmand@gmail.com>
2024-12-24 10:26:57 +0330

    Add ApplicationDbContext and EF Core packages

---
+++ src/ShortLink.Infrastructure.Persistence/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShortLink.Domain.Entities;
+
+namespace ShortLink.Infrastructure.Persistence
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<UrlAddress> UrlAddresses { get; set; }
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.; Database=ShortLink; Trusted_Connection=True;");
+        }
+
+
+    }
+}
