05074e72a56280267f62203550fddda43ef94f63
zahra liaghatmand <zahra.liaghatmand@gmail.com>
2025-01-20 12:05:49 +0330

    Add simple Result Pattern

---
+++ src/ShortLink.Shared/Result/Error.cs
@@ -0,0 +1,25 @@
+﻿namespace ShortLink.Shared.Result
+{
+    public readonly record struct Error
+    {
+        private readonly string _message { get; }
+        private readonly string _description { get; }
+        private readonly ErrorType _type { get; }
+        private Error(ErrorType type, string message, string description)
+        {
+            _type = type;
+            _message = message;
+            _description = description;
+        }
+        public static Error None(string message, string description)
+            => new Error(ErrorType.None, message, description);
+        public static Error Failure(string message, string description)
+            => new Error(ErrorType.Failure, message, description);
+        public static Error Validation(string message, string description)
+            => new Error(ErrorType.Validation, message, description);
+        public static Error Unauthorized(string message, string description)
+            => new Error(ErrorType.Unauthorized, message, description);
+        public static Error NotFound(string message, string description)
+            => new Error(ErrorType.NotFound, message, description);
+    }
+}
---
+++ src/ShortLink.Shared/Result/Result.cs
@@ -0,0 +1,35 @@
+﻿using ShortLink.Shared.Result;
+
+namespace ShortLink.Shared
+{
+    public readonly record struct Result<T>
+    {
+        private readonly T? _value = default;
+        private readonly List<Error>? _errors = [];
+        private bool HasErrors => _errors.Count > 0;
+        public Result(T value)
+        {
+            _value = value;           
+        }
+        public Result(Error error)
+        {
+            _errors.Add(error);
+            _errors = [];
+        }
+        public T Value { 
+            get
+            {
+                if(HasErrors == true) { throw new Exception(); }
+                return _value;
+            } 
+        }
+        public static implicit operator Result<T>(T value)
+        {
+            return new Result<T>(value);
+        }
+        public static implicit operator Result<T>(Error error)
+        {
+            return new Result<T>(error);
+        }
+    }
+}
