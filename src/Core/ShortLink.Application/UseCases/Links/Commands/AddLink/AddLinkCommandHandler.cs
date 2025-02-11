76aef22acc9d304d9a178f4db4f64820446db33a
zahra liaghatmand <zahra.liaghatmand@gmail.com>
2025-02-11 14:58:20 +0330

    Add a short code generator and interface for it

--- src/Core/ShortLink.Application/UseCases/Links/Commands/AddLink/AddLinkCommandHandler.cs
+++ src/Core/ShortLink.Application/UseCases/Links/Commands/AddLink/AddLinkCommandHandler.cs
@@ -6,22 +6,25 @@ using ShortLink.Shared.Result;
 
 namespace ShortLink.Application.UseCases.Links.Commands.AddLink
 {
-    public class AddLinkCommandHandler(IApplicationDbContext dbContext) : IRequestHandler<AddLinkCommand, Result<LinksDto>>
+    public class AddLinkCommandHandler(IApplicationDbContext dbContext,
+        IShortCodeGenerator shortCodeGenerator) : IRequestHandler<AddLinkCommand, Result<LinksDto>>
     {
         private readonly IApplicationDbContext _dbContext = dbContext;
+        private readonly IShortCodeGenerator _shortCodeGenerator = shortCodeGenerator;
         public async Task<Result<LinksDto>> Handle(AddLinkCommand request, CancellationToken cancellationToken = default)
         {
+            string shortCode = _shortCodeGenerator.Generator(request.Url);
             Link link = new()
             {
-                Owner = new() { UserName = "zz" },
-                ShortCode = "www",
+                Owner = new() { UserName = "Zahra" },
+                ShortCode = shortCode,
                 Url = request.Url
             };
             var newLink = _dbContext.Links.Add(link);
             Result saveChangesResult = await _dbContext.SaveChangesAsync(cancellationToken);
             return saveChangesResult.IsFailure
                     ? Result.Failure<LinksDto>(Error.Failure("Save changes failed", "Save changes failed"))
-                    : Result.Success(new LinksDto { ShortCode = "www", Url = "www.gg.com" });
+                    : Result.Success(new LinksDto { ShortCode = shortCode, Url = request.Url });
         }
     }
 }
